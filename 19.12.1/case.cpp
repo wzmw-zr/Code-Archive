d3df20d65894cb49b0ddb9fa9e5ca266ae2b4ff4
wzmw-zr <1767153298@qq.com>
2019-12-02 00:46:33 +0800

    2019.12.01

---
+++ 19.12.1/case.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: case.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2019年12月01日 星期日 18时46分59秒
+ ************************************************************************/
+
+#include<iostream>
+#include<cstdio>
+using namespace std;
+
+int main () {
+    int n;
+    cin >> n;
+    cout << (n >> 1);
+    return 0;
+}
