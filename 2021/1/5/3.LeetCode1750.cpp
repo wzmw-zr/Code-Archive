92bf633b8eb65517effcdd3e0aa0cec658ec57be
wzmw-zr <1767153298@qq.com>
2021-01-05 22:00:33 +0800

    2021.01.05

--- 2021/1/5/3.LeetCode1750.cpp
+++ 2021/1/5/3.LeetCode1750.cpp
@@ -1,11 +1,13 @@
 /*************************************************************************
-	> File Name: 2.LeetCode1750.cpp
+	> File Name: 3.LeetCode1750.cpp
 	> Author: 
 	> Mail: 
-	> Created Time: 2021年01月05日 星期二 21时04分57秒
+	> Created Time: 2021年01月05日 星期二 21时19分46秒
  ************************************************************************/
 
 #include<iostream>
+#include<cstring>
+#include<cstdlib>
 #include<string>
 #include<vector>
 #include<queue>
@@ -18,20 +20,35 @@
 using namespace std;
 
 int dir[4][2] = {1, 0, -1, 0, 0, 1, 0, -1};
+bool handled[201][201][201];
+bool result[201][201][201];
 
 bool dfs(vector<vector<char>> &board, vector<vector<bool>> &check, int x, int y, string &word, int ind) {
     if (x < 0 || x >= board.size() || y < 0 || y >= board[0].size()) return false;
     if (check[x][y]) return false;
-    if (!word[ind]) return true;
-    if (board[x][y] != word[ind]) return false;
+    if (handled[x][y][ind]) return  result[x][y][ind];
+    if (board[x][y] != word[ind]) {
+        handled[x][y][ind] = true;
+        result[x][y][ind] = false;
+        return false;
+    } 
+    if (ind == (int) word.size() - 1) {
+        handled[x][y][ind] = true;
+        result[x][y][ind] = true;
+        return true;
+    }
     bool ans = false;
     check[x][y] = true;
     for (int i = 0; i < 4 && !ans; i++) ans |= dfs(board, check, x + dir[i][0], y + dir[i][1], word, ind + 1);
     check[x][y] = false;
+    handled[x][y][ind] = true;
+    result[x][y][ind] = ans;
     return ans;
 }
 
 bool exist(vector<vector<char>>& board, string word) {
+    memset(handled, 0, sizeof(handled));
+    memset(result, 0, sizeof(result));
     int h = board.size(), w = board[0].size();
     vector<vector<bool>> check(h, vector<bool>(w, false));
     for (int i = 0; i < h; i++) {
