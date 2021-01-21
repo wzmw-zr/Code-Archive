9224bb2ee459dce3472c112e60b73fdd1a9530a2
wzmw-zr <1767153298@qq.com>
2021-01-21 23:34:18 +0800

    2021.01.21

--- 2021/1/21/1.LeetCode1489.cpp
+++ 2021/1/21/1.LeetCode1489.cpp
@@ -18,13 +18,14 @@
 using namespace std;
 
 struct Edge {
+    int ind;
     int from;
     int to;
     int weight;
     Edge() = default;
-    Edge(int from, int to, int weight) : from(from), to(to), weight(weight) {}
+    Edge(int ind, int from, int to, int weight) : ind(ind), from(from), to(to), weight(weight) {}
     bool operator<(const struct Edge &a) const {
-        return this->weight < a.weight;
+        return this->weight > a.weight;
     }
 };
 
@@ -53,16 +54,19 @@ struct UnionSet {
     }
 };
 
-int kruscal(UnionSet &u, vector<vector<int>> &edges, vector<bool> &check, int &m) {
+int kruscal(UnionSet &u, vector<vector<int>> &edges, vector<bool> &check, int &m, vector<bool> &appear, vector<int> &indices) {
     int ans = 0;
     priority_queue<Edge> que;
     for (int i = 0; i < m; i++) {
-        if (check[i]) que.push(Edge(edges[i][0], edges[i][1], edges[i][2]));
+        if (check[i]) que.push(Edge(i, edges[i][0], edges[i][1], edges[i][2]));
     }
     while (!que.empty() && (u.cnt > 1)) {
         Edge temp = que.top();
         que.pop();
-        if (u.merge(temp.from, temp.to)) ans += temp.weight;
+        if (u.merge(temp.from, temp.to)) {
+            ans += temp.weight;
+            indices.push_back(temp.ind);
+        }
     }
     return u.cnt == 1 ? ans : -1;
 }
@@ -70,18 +74,28 @@ int kruscal(UnionSet &u, vector<vector<int>> &edges, vector<bool> &check, int &m
 vector<vector<int>> findCriticalAndPseudoCriticalEdges(int n, vector<vector<int>>& edges) {
     UnionSet u(n);
     int m = edges.size();
-    vector<bool> check(n, true);
-    int mst_val = kruscal(u, edges, check, m);
-    u.clear();
+    vector<bool> check(m, true);
+    vector<bool> appear(m, false);
+    vector<int> indices;
+    int mst_val = kruscal(u, edges, check, m, appear, indices);
+    for (int &x : indices) appear[x] = true;
     vector<int> critical;
     vector<int> pseudo_critical;
     for (int i = 0; i < m; i++) {
         check[i] = false;
-        int t = kruscal(u, edges, check, m);
         u.clear();
-        if (t == -1 || t > mst_val) critical.push_back(i);
-        else pseudo_critical.push_back(i);
+        indices.clear();
+        int t = kruscal(u, edges, check, m, appear, indices);
+        u.clear();
+        if (t == -1 || t > mst_val) critical.push_back(i); 
+        else {
+            if (t == mst_val) for (int &x : indices) appear[x] = true;
+        }
         check[i] = true;
+    }
+    for (int &x : critical) appear[x] = false;
+    for (int i = 0; i < m; i++) {
+        if (appear[i]) pseudo_critical.push_back(i);
     }
     vector<vector<int>> res;
     res.push_back(critical);
