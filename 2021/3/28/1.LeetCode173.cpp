52c97b6c7bfa3d9da1968e017885c3b288c9b09a
wzmw-zr <1767153298@qq.com>
2021-03-28 00:07:40 +0800

    2021.03.28

---
+++ 2021/3/28/1.LeetCode173.cpp
@@ -0,0 +1,57 @@
+/*************************************************************************
+	> File Name: 1.LeetCode173.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2021年03月28日 星期日 00时01分50秒
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<queue>
+#include<stack>
+#include<map>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<algorithm>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class BSTIterator {
+public:
+    vector<int> nums;
+    int ind;
+
+    BSTIterator(TreeNode* root) {
+        ind = 0;
+        dfs(root);
+    }
+
+    void dfs(TreeNode *root) {
+        if (!root) return ;
+        dfs(root->left);
+        nums.push_back(root->val);
+        dfs(root->right);
+    }
+
+    int next() {
+        return nums[ind++];
+    }
+
+    bool hasNext() {
+        return ind < (int) nums.size() - 1;
+    }
+};
+
+
+int main() {
+    return 0;
+}
