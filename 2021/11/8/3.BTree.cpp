904139bce88a2d624b9bf01a8223ef0be0525221
wzmw-zr <1767153298@qq.com>
2021-11-08 15:49:20 +0800

    2021.11.08

---
+++ 2021/11/8/3.BTree.cpp
@@ -0,0 +1,252 @@
+/*************************************************************************
+	> File Name: 3.BTree.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2021年11月08日 星期一 14时13分58秒
+ ************************************************************************/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cstring>
+#include <cstdlib>
+#include <tuple>
+using namespace std;
+
+const int M = 2;
+
+struct Node {
+    int cnt;
+    int vals[2 * M + 1];
+    Node *next[2 * M + 2];
+
+    Node() : cnt(0) {
+        memset(vals, 0, sizeof(vals));
+        memset(next, 0, sizeof(next));
+    }
+
+    void insert(int ind, int val) {
+        for (int i = cnt - 1; i >= ind; i--) vals[i + 1] =  vals[i];
+        for (int i = cnt; i >= ind; i--) next[i + 1] = next[i];
+        vals[ind] = val;
+        cnt++;
+    }
+
+    void erase(int ind) {
+        if (ind < cnt - 1) {
+            for (int i = ind; i < cnt - 1; i++) vals[i] = vals[i + 1];
+            for (int i = ind; i < cnt; i++) next[i] = next[i + 1];
+        }
+        vals[cnt - 1] = 0;
+        next[cnt] = nullptr;
+        cnt--;
+    }
+
+    void clear() {
+        cnt = 0;
+        memset(vals, 0, sizeof(vals));
+        memset(next, 0, sizeof(next));
+    }
+};
+
+ostream &operator<<(ostream &out, Node *node) {
+    out << "(";
+    for (int i = 0; i < node->cnt; i++) out << node->vals[i] << " ";
+    out << ")";
+    return out;
+}
+
+struct BTree {
+    Node *_root;
+
+    BTree() : _root(new Node()) {}
+
+    bool search(int val) {
+        return search(_root, val);
+    }
+
+    int get_ind(Node *root, int val) {
+        int l = 0, r = root->cnt;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (root->vals[mid] < val) l = mid + 1;
+            else r = mid;
+        }
+        return l;
+    }
+
+    bool search(Node *root, int val) {
+        if (!root) return false;
+        int ind = get_ind(root, val);
+        if (root->vals[ind] == val) return true;
+        return search(root->next[ind], val);
+    }
+
+    void insert(int val) {
+        _root = insert(_root, val);
+        if (_root->cnt == (2 * M + 1)) {
+            auto &&[left, right, mid] = split(_root);
+            _root->clear();
+            _root->vals[0] = mid;
+            _root->cnt++;
+            _root->next[0] = left;
+            _root->next[1] = right;
+        }
+    }
+
+    Node *insert(Node *root, int val) {
+        if (!root) return root;
+        int ind = get_ind(root, val);
+        if (root->vals[ind] == val) return root;
+        if (!root->next[ind]) root->insert(ind, val);
+        else root->next[ind] = insert(root->next[ind], val);
+        root = maintain(root, ind);
+        return root;
+    }
+
+    tuple<Node *, Node *, int> split(Node *node) {
+        cout << "split : " << node << endl;
+        int mid = node->vals[M];
+        Node *left = new Node();
+        Node *right = new Node();
+        for (int i = 0; i < M; i++) left->vals[i] = node->vals[i];
+        for (int i = 0; i <= M; i++) left->next[i] = node->next[i];
+        left->cnt = M;
+        for (int i = 0; i < M; i++) right->vals[i] = node->vals[i + M + 1];
+        for (int i = 0; i <= M; i++)  right->next[i] = node->next[i + M + 1];
+        right->cnt = M;
+        return {left, right, mid};
+    }
+
+    Node *merge(int mid, Node *left, Node *right) {
+        cout << "merge " << "mid = " << mid << "left = " << left << " right = " << right << endl;
+        Node *temp = new Node();
+        for (int i = 0; i < left->cnt; i++) temp->vals[i] = left->vals[i];
+        temp->vals[left->cnt] = mid;
+        for (int i = 0; i < right->cnt; i++) temp->vals[i + left->cnt + 1] = right->vals[i];
+        temp->cnt = left->cnt + right->cnt + 1;
+        for (int i = 0; i <= left->cnt; i++) temp->next[i] = left->next[i];
+        for (int i = 0; i <= right->cnt; i++) temp->next[i + left->cnt + 1] = right->next[i];
+        return temp;
+    }
+    
+    Node *maintain(Node *root, int ind) {
+        if (!root || !root->next[ind]) return root;
+        cout << root <<  " ind = " << ind << endl;
+        if (root->next[ind]->cnt == (2 * M + 1)) {
+            auto &&[left, right, mid] = split(root->next[ind]);
+            root->insert(ind, mid);
+            delete root->next[ind];
+            root->next[ind] = left;
+            root->next[ind + 1] = right;
+            return root;
+        }
+        if (root->next[ind]->cnt == (M - 1)) {
+            if (ind < root->cnt) {
+                if (root->next[ind + 1]->cnt > M) {
+                    cout << root->next[ind] << endl;
+                    root->next[ind]->insert(M - 1, root->vals[ind]);
+                    cout << root->next[ind] << endl;
+                    root->vals[ind] = root->next[ind + 1]->vals[0];
+                    root->next[ind]->next[M] = root->next[ind + 1]->next[0];
+                    root->next[ind + 1]->erase(0);
+                } else {
+                    Node *temp = merge(root->vals[ind], root->next[ind], root->next[ind + 1]);
+                    delete root->next[ind];
+                    delete root->next[ind + 1];
+                    root->erase(ind);
+                    root->next[ind] = temp;
+                }
+            } else {
+                if (root->next[ind - 1]->cnt > M) {
+                    int t = root->next[ind - 1]->cnt;
+                    root->next[ind]->insert(0, root->vals[ind - 1]);
+                    root->vals[ind - 1] = root->next[ind - 1]->vals[t - 1];
+                    root->next[ind]->next[0] = root->next[ind - 1]->next[t];
+                    root->next[ind - 1]->erase(t - 1);
+                } else {
+                    Node *temp = merge(root->vals[ind - 1], root->next[ind - 1], root->next[ind]);
+                    delete root->next[ind - 1];
+                    delete root->next[ind];
+                    root->erase(root->cnt - 1);
+                    root->next[root->cnt] = temp;
+                }
+            }
+        }
+        return root;
+    }
+
+    int predecessor(Node *root, int ind) {
+        Node *temp = root->next[ind];
+        while (temp->next[temp->cnt]) temp = temp->next[temp->cnt];
+        return temp->vals[temp->cnt - 1];
+    }
+
+    void level_print() {
+        using PNI = pair<Node *, int>;
+        queue<PNI> que;
+        que.push(PNI(_root, 0));
+        while (!que.empty()) {
+            auto temp = que.front();
+            que.pop();
+            cout << temp.first << " " << temp.second << endl;
+            for (int i = 0; i <= temp.first->cnt; i++) {
+                if (!temp.first->next[i]) break;
+                que.push(PNI(temp.first->next[i], temp.second + 1));
+            }
+        }
+    }
+
+    void erase(int val) {
+        _root = erase(_root, val);
+        if ((_root->cnt == 0) && _root->next[0]) {
+            Node *temp = _root->next[0];
+            delete _root;
+            _root = temp;
+        }
+    }
+
+    Node *erase(Node *root, int val) {
+        if (!root) return root;
+        int ind = get_ind(root, val);
+        if (root->vals[ind] == val) {
+            if (!root->next[ind]) {
+                cout << root << endl;
+                root->erase(ind);
+                cout << root << endl;
+            }
+            else {
+                int pre = predecessor(root, ind);
+                root->vals[ind] = pre;
+                root->next[ind] = erase(root->next[ind], pre);
+            }
+        } else root->next[ind] = erase(root->next[ind], val);
+        root = maintain(root, ind);
+        return root;
+    }
+};
+
+
+int main() {
+    BTree btree;
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) btree.insert(i);
+    btree.level_print();
+    cout << endl;
+    while (1) {
+        cout << "value to erase : ";
+        cin >> n;
+        btree.erase(n);
+        btree.level_print();
+        cout << endl;
+    }
+    return 0;
+}
