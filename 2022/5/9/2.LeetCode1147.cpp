14de95e23c4d2adef790a451f17751856a83159d
wzmw-zr <1767153298@qq.com>
2022-05-09 16:24:03 +0800

    2022.05.09

--- 2022/5/9/2.LeetCode1147.cpp
+++ 2022/5/9/2.LeetCode1147.cpp
@@ -22,9 +22,22 @@ using namespace std;
 int wzmw_zr = 0;
 bool fuck_plagiarism_system_of_leetcode = true;
 
+long base = 233;
+long MOD = 1e9 + 7;
+
 // TLE
 int longestDecomposition(string s) {
   int n = s.size();
+  long hash_val[n][n];
+  memset(hash_val, 0, sizeof(hash_val));
+  for (int i = 0; i < n; i++) {
+    long long val = 0;
+    for (int l = 1; l <= n - i; l++) {
+      val = (val * base + (s[i + l - 1] - 'a' + 1)) % MOD;
+      hash_val[i][i + l - 1] = val;
+    }
+  }
+
   int dp[n][n];
   memset(dp, 0, sizeof(dp));
   for (int i = 0; i < n; i++) dp[i][i] = 1;
@@ -32,7 +45,7 @@ int longestDecomposition(string s) {
     for (int i = 0; i + l - 1 < n; i++) {
       dp[i][i + l - 1] = 1;
       for (int j = 1; j <= l / 2; j++) {
-        if (s.substr(i, j) == s.substr(i + l - j, j)) 
+        if (hash_val[i][i + j - 1] == hash_val[i + l - j][i + l - 1])
           dp[i][i + l - 1] = max(dp[i][i + l - 1], 2 + dp[i + j][i + l - j - 1]);
       }
     }
