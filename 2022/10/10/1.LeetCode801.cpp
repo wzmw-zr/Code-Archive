a532613a35c1a7f5f575151ab4d6ddcb0cb52991
wzmw-zr <1767153298@qq.com>
2022-10-10 09:16:31 +0800

    2022.10.10

---
+++ 2022/10/10/1.LeetCode801.cpp
@@ -0,0 +1,45 @@
+/*************************************************************************
+	> File Name: 1.LeetCode801.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2022年10月10日 星期一 09时10分35秒
+ ************************************************************************/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cstring>
+#include <cstdlib>
+using namespace std;
+
+int wzmw_zr = 0;
+bool fuck_plagiarism_system_of_leetcode = true;
+
+int minSwap(vector<int>& nums1, vector<int>& nums2) {
+  int n = nums1.size();
+  int dp[n][2];
+  memset(dp, 0x3f, sizeof(dp));
+  dp[0][0] = 0, dp[0][1] = 1;
+  for (int i = 1; i < n; i++) {
+    if (nums1[i] > nums1[i - 1] && nums2[i] > nums2[i - 1]) {
+      dp[i][0] = min(dp[i][0], dp[i - 1][0]);
+      dp[i][1] = min(dp[i][1], dp[i - 1][1] + 1);
+    }
+    if (nums1[i] > nums2[i - 1] && nums2[i] > nums1[i - 1]) {
+      dp[i][0] = min(dp[i][0], dp[i - 1][1]);
+      dp[i][1] = min(dp[i][1], dp[i - 1][0] + 1);
+    }
+  }
+  return min(dp[n - 1][0], dp[n - 1][1]);
+}
+
+int main() {
+  return 0;
+}
